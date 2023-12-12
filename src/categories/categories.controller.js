be321ff1fa1463b70cabdea1f339c9c8c16b4a36
Will Williams <wwilliams@indeed.com>
2023-12-11 19:47:02 -0800

    CRUD: Adds services to contain 'read' and 'list' db logic for Products, Categories, then updates controllers to use new services

--- src/categories/categories.controller.js
+++ src/categories/categories.controller.js
@@ -1,11 +1,10 @@
-async function list(req, res, next) {
-  res.json({
-    data: [
-      { category_name: "category 1" },
-      { category_name: "category 2" },
-      { category_name: "category 3" },
-    ],
-  });
+const categoriesService = require("./categories.service");
+
+function list(req, res, next) {
+  categoriesService
+    .list()
+    .then((data) => res.json({ data }))
+    .catch(next);
 }
 
 module.exports = {
